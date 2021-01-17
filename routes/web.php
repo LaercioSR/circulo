c3f8efd376d905f7ab197fb472a862b4d17e82fa
di3goCS <diego.cs282@gmail.com>
2021-01-17 13:40:52 -0300

    adiciona seeders

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/*Route::get('/', function () {
+Route::get('/', function () {
     return view('students.courses');
-});*/
+});
 
 Route::get('/courses', 'App\Http\Controllers\CourseController@index')->name('course.index');
 Route::get('/mentoring/{id}', 'App\Http\Controllers\MentoringController@index')->name('mentoring.index');
