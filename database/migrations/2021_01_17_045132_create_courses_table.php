7449b537863d6b58e02ecd996e0a8addf4e3f364
di3goCS <diego.cs282@gmail.com>
2021-01-17 03:30:52 -0300

    adiciona interesses e cursos

--- database/migrations/2021_01_17_045132_create_courses_table.php
+++ database/migrations/2021_01_17_045132_create_courses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateJobsTable extends Migration
+class CreateCoursesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,13 @@ class CreateJobsTable extends Migration
      */
     public function up()
     {
-        Schema::create('jobs', function (Blueprint $table) {
+        Schema::create('courses', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->string('description');
+            $table->integer('duration');
+            $table->string('platform');
             $table->timestamps();
-
-            $table->foreignId('company_id')->constrained()->onDelete('cascade');
-            $table->foreignId('city_id')->constrained()->onDelete('cascade');
-            $table->foreignId('state_id')->constrained()->onDelete('cascade');
         });
     }
 
@@ -32,6 +30,6 @@ class CreateJobsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('jobs');
+        Schema::dropIfExists('courses');
     }
 }
